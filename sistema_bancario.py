6a958b7358e2affca7243a4b2cc20771425ec6ca
Mariana Pinheiro <pinheiromariana896@gmail.com>
2023-07-26 16:14:19 -0300

    SegundoCommit

---
+++ sistema_bancario.py
@@ -0,0 +1,71 @@
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+=>
+"""
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+
+
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito"))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou: o valor informado é inválido")
+
+    elif opcao == "s":
+        valor + float(input("informe o valor do saque"))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou, saldo insuficiente")
+
+        elif excedeu_limite:
+            print("Operação falhou, o valor do saque excedeu o limite")
+
+        elif excedeu_saques:
+            print("Operação falhou, número de saques excedido")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou!O valor informado é inválido")
+
+    
+    elif opcao == "q":
+        break
+
+    elif opcao == "e":
+        print("\n          Extrato          ")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\n Saldo: R$ {saldo:.2f}")
+
+    else:
+        print("Selecione uma opção desejada")
+
+
+
+
+        
